ca8dbfea1d1c1115b8f9ab3bb18799439e4e92a2
BangL <henno.rickowski@gmail.com>
2017-07-02 08:45:05 +0200

    fix: use vanilla weapon names when clicking the "Purchase on community market" button on weapon skins, so it actually gets results on the market

--- lua/Blackmarket.lua
+++ lua/Blackmarket.lua
@@ -66,3 +66,14 @@ Hooks:PostHook( BlackMarketTweakData , "_init_melee_weapons" , "RWN_BlackMarketT
 		end
 	end
 end )
+
+Hooks:PostHook( EconomyTweakData , "create_weapon_skin_market_search_url" , "RWN_EconomyTweakDataPostCreateWeaponSkinMarketSearchUrl" , function( self , weapon_id, cosmetic_id )
+	local cosmetic_name = tweak_data.blackmarket.weapon_skins[cosmetic_id] and managers.localization:text(tweak_data.blackmarket.weapon_skins[cosmetic_id].name_id)
+	local weapon_name = managers.localization:text(tweak_data.weapon[weapon_id].name_id .. "__BACKUP")
+	if cosmetic_name and weapon_name then
+		cosmetic_name = string.gsub(cosmetic_name, " ", "+")
+		weapon_name = string.gsub(weapon_name, " ", "+")
+		return string.gsub("http://steamcommunity.com/market/search?appid=218620&q=" .. cosmetic_name .. "+" .. weapon_name, "++", "+")
+	end
+	return nil
+end )
