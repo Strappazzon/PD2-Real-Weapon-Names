61c1e9b9fcdd0d2f7c5677250229e92b25d7be36
shinrax2 <shinra.ubu1@yahoo.de>
2018-06-30 16:56:30 +0200

    added support for chinese and thai custom languages

--- lua/RWN.lua
+++ lua/RWN.lua
@@ -13,15 +13,31 @@ Hooks:Add("LocalizationManagerPostInit", "RWNLoc_LocalizationManagerPostInit", f
         ["french"] = "fr",
         ["italian"] = "it",
         ["russian"] = "ru",
+		["thai"] = "th"
     }
     for k,v in pairs(supported_languages) do
         if Idstring(k):key() == SystemInfo:language():key() then
-            log("RWN: Current language is: " .. v)
             current_language = v
         end
     end
+	--custom language stuff thx to NewPJzuza for help
+	for _, mod in pairs(BLT and BLT.Mods:Mods() or {}) do
+			if mod:GetName() == "PAYDAY 2 THAI LANGUAGE Mod" and mod:IsEnabled() then
+				current_language = "th" 
+				break
+			end
+			if mod:GetName() == "ChnMod (Patch)" and mod:IsEnabled() then
+				current_language = "ch" 
+				break
+			end	
+			if mod:GetName() == "Ultimate Localization Manager & 正體中文化" and mod:IsEnabled() then
+				current_language = "ch" 
+				break
+			end		
+	end
 
     if current_language then -- Controlla che la lingua sia stata identificata nel modo corretto
+		log("RWN: Current language is: " .. current_language)
         local _path = RWNLoc._path .. current_language .. ".txt"
 
         if io.file_is_readable(_path) then -- Il file esiste?
