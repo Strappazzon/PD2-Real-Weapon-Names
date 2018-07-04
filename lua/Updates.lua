77f6a0c6a257e91ba3fc45b407c2a02ce9ff69da
shinrax2 <shinra.ubu1@yahoo.de>
2018-07-04 15:46:59 +0200

    updated Updates.lua

--- lua/Updates.lua
+++ lua/Updates.lua
@@ -1,4 +1,4 @@
-superblt = false
+local superblt = false
 for _, mod in pairs(BLT and BLT.Mods:Mods() or {}) do
 	if mod:GetName() == "SuperBLT" and mod:IsEnabled() then
 		superblt = true
@@ -11,7 +11,7 @@ if superblt == false then
 			mod_id = 'Real Weapon Names',
 			data = {
 				modworkshop_id = 19958,
-				dl_url = 'https://github.com/xDarkWolf/PD2-Real-Weapon-Names/blob/master/RWN.zip?raw=true',
+				dl_url = 'https://raw.githubusercontent.com/xDarkWolf/PD2-Real-Weapon-Names/blob/master/RWN.zip',
 				info_url = 'https://raw.githubusercontent.com/xDarkWolf/PD2-Real-Weapon-Names/master/mod.txt'
 			}
 		})
