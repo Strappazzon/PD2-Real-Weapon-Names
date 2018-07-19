1aa3079015b50026662426f7f0f1bf1ee6003f03
shinrax2 <shinra.ubu1@yahoo.de>
2018-07-19 17:19:08 +0200

    v2.8.1

--- dev/test_strings.lua
+++
@@ -1,9 +0,0 @@
-local original_text = LocalizationManager.text
-local test_strings = false
-
-function LocalizationManager:text(string_id, ...)
-
-return test_strings == true and string_id or
-	original_text(self, string_id, ...)
-
-end
