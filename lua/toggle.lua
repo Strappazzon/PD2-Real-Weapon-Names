d1e16a1f1d827746e91a639d66904ce3ac014877
shinrax2 <shinra.ubu1@yahoo.de>
2018-09-19 03:12:07 +0200

    added keybind to display vanilla names for achievement requirement guns

---
+++ lua/toggle.lua
@@ -0,0 +1,11 @@
+--from Snh20's string_id Revealer mod
+--https://modworkshop.net/mydownloads.php?action=view_down&did=14801
+if not StringIDReveal_InChat or StringIDReveal_InChat() then
+	return
+end
+
+if String_ShowTranslated == nil then
+	String_ShowTranslated = false
+else
+	String_ShowTranslated = not String_ShowTranslated
+end
