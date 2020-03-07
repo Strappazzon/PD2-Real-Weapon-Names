0ab1b63d9537327e1d33c69f52eacb8587593ed3
Strappazzon <strappazzon@protonmail.com>
2020-03-07 23:41:44 +0100

    Small changes to test script

--- script/test-mod.ps1
+++ script/test-mod.ps1
@@ -4,7 +4,7 @@ $PaydayExe = Get-Process 'payday2_win32_release' -ErrorAction SilentlyContinue
 $DevFolder = (Get-Item (Split-Path $PSCommandPath -Parent)).Parent.FullName
 
 # Run the script only if PAYDAY 2 is installed
-if([System.IO.Directory]::Exists(${PaydayPath})) {
+if ([System.IO.Directory]::Exists(${PaydayPath})) {
 	# Close PAYDAY 2 if it's running
 	if (${PaydayExe}) {
 		${PaydayExe}.CloseMainWindow()
@@ -18,9 +18,8 @@ if([System.IO.Directory]::Exists(${PaydayPath})) {
 	Remove-Item ${PaydayPath}'\mods\Real Weapon Names\*' -Recurse
 
 	# Copy the mod files inside the game's "mods" folder
-	New-Item -ItemType Directory -Path ${PaydayPath}'\mods\Real Weapon Names\lua' | Out-Null
-	Copy-Item -Path ${DevFolder}'\lua\*.lua' -Destination ${PaydayPath}'\mods\Real Weapon Names\lua\' -Recurse | Out-Null
 	New-Item -ItemType Directory -Path ${PaydayPath}'\mods\Real Weapon Names\lua\loc' | Out-Null
+	Copy-Item -Path ${DevFolder}'\lua\*.lua' -Destination ${PaydayPath}'\mods\Real Weapon Names\lua\' -Recurse | Out-Null
 	Copy-Item -Path ${DevFolder}'\lua\loc\*.json' -Destination ${PaydayPath}'\mods\Real Weapon Names\lua\loc\' -Recurse | Out-Null
 	Copy-Item -Path ${DevFolder}'\mod.txt' -Destination ${PaydayPath}'\mods\Real Weapon Names' | Out-Null
 	Copy-Item -Path ${DevFolder}'\RWN.png' -Destination ${PaydayPath}'\mods\Real Weapon Names' | Out-Null
@@ -29,7 +28,7 @@ if([System.IO.Directory]::Exists(${PaydayPath})) {
 	# Launching the game from the shell will make SuperBLT create logs inside ${DevFolder}
 	# Start-Process ${PaydayPath}'\payday2_win32_release.exe'
 	Start-Process 'steam://run/218620'
-
-	# Quit the script
-	exit
 }
+
+# Quit the script
+exit
